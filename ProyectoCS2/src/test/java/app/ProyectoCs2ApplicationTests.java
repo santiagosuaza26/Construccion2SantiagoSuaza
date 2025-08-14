3fed057754aeca6533785a9316b0c8bfa6d3bf46
suaza <santiagosuaza5@gmail.com>
2025-08-13 19:59:36 -0500

    intial commit

---
+++ ProyectoCS2/src/test/java/app/ProyectoCs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
