c45f285987936c936f45ea100feaba58049cf8ad
suaza <santiagosuaza5@gmail.com>
2025-09-02 18:56:02 -0500

    v2.0

--- ProyectoCS2/src/main/java/app/domain/model/InventoryItem.java
+++ ProyectoCS2/src/main/java/app/domain/model/InventoryItem.java
@@ -5,11 +5,13 @@ public abstract class InventoryItem {
     private String id;
     private String name;
     private double cost;
+    private int quantity;
 
-    public InventoryItem(String id, String name, double cost) {
+    public InventoryItem(String id, String name, double cost, int quantity) {
         this.id = id;
         this.name = name;
         this.cost = cost;
+        this.quantity = quantity;
     }
 
     public String getId() {
@@ -25,7 +27,7 @@ public abstract class InventoryItem {
     }
 
     public int getQuantity() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return quantity;
     }
 }
 
